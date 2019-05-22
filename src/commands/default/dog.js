9336e028ecb2b46bf5fbc1684490f75ef9d9a7e8
minemidnight <minemidnightmc@gmail.com>
2019-05-22 19:11:30 -0400

    Add debug message

--- src/commands/default/dog.js
+++ src/commands/default/dog.js
@@ -6,6 +6,13 @@ module.exports = {
 		const { body: { url: file } } = await superagent.get("https://random.dog/woof.json");
 		const { body: buffer } = await superagent.get(file);
 
+		console.log("packet", {
+			channelId: ctx.channelID,
+			file: {
+				name: path.basename(file),
+				file: buffer
+			}
+		});
 		await ctx.bucket.request("createChannelMessage", {
 			channelId: ctx.channelID,
 			file: {
