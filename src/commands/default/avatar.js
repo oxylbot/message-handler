b6a708d3eb2ff6e8dd684ddd71eb004ed65ae126
minemidnight <minemidnightmc@gmail.com>
2019-06-23 18:31:57 -0400

    Add debug message

--- src/commands/default/avatar.js
+++ src/commands/default/avatar.js
@@ -3,12 +3,14 @@ const superagent = require("superagent");
 
 module.exports = {
 	async run(ctx) {
+		console.log(ctx.args);
 		const user = ctx.args[0] || await ctx.gatewayRequest().discord().users().get(ctx.authorID);
 		const avatarURL = user.avatar ?
 			`https://cdn.discordapp.com/avatars/${user.id}/` +
 				`${user.avatar}.${user.avatar.startsWith("a_") ? "gif" : "png"}?size=1024` :
 			`https://cdn.discordapp.com/embed/avatars/${parseInt(user.discriminator) % 5}.png`;
 
+		console.log(avatarURL);
 		const { body: avatar } = await superagent.get(avatarURL);
 		await ctx.bucket.request("createChannelMessage", {
 			channelId: ctx.channelID,
