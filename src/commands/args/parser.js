b736cda9b58ef542067ea943b9aa1803da16151f
minemidnight <minemidnightmc@gmail.com>
2019-07-20 19:03:04 -0400

    Fix arg resolving for last arg

--- src/commands/args/parser.js
+++ src/commands/args/parser.js
@@ -7,20 +7,19 @@ module.exports = async (command, ctx) => {
 	for(let i = 0; i < command.args.length; i++) {
 		const commandArg = command.args[i];
 		let [arg] = regex.exec(ctx.rawArgs) || [null];
+		if(i === command.args.length - 1) arg += ctx.rawArgs.substring(regex.lastIndex);
 		if(arg !== null && arg.startsWith("\"") && arg.endsWith("\"")) arg = arg.slice(1, -1);
 
 		const expected = commandArg.label === commandArg.type ?
 			commandArg.label :
 			`${commandArg.label} as ${commandArg.type}`;
 
-		if(arg === null && commandArg.optional) {
-			continue;
-		} else if(arg === null && commandArg.default !== undefined) {
+		if(arg === null && commandArg.default !== undefined) {
 			arg = commandArg.default;
+		} else if(arg === null && commandArg.optional) {
+			continue;
 		} else if(arg === null) {
 			throw new Error(`Missing argument #${i + 1}; expected ${expected}`);
-		} else if(i === command.args.length - 1) {
-			arg += ctx.rawArgs.substring(regex.lastIndex);
 		} else {
 			try {
 				arg = await resolver[commandArg.type](ctx, commandArg, arg);
