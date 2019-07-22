9d3787baa406257e365f5510c9066908af872b10
minemidnight <minemidnightmc@gmail.com>
2019-07-22 18:56:10 -0400

    Remove debug messages

--- src/commands/args/user.js
+++ src/commands/args/user.js
@@ -1,12 +1,9 @@
 module.exports = async (ctx, arg, input) => {
-	console.log("Resolving user...");
 	const [, id] = input.match(/<@!?(\d{15,21})>/) || [undefined, undefined];
 	if(id || /\d{15,21}/.test(input)) {
-		console.log("Mentions or is ID");
 		try {
 			return await ctx.gatewayRequest().discord().users().get(id);
 		} catch(err) {
-			console.log("Gateway request errored");
 			if(err.resp.status === 404) throw new Error("User not found");
 			else throw err;
 		}
@@ -14,7 +11,6 @@ module.exports = async (ctx, arg, input) => {
 		let discrim = false;
 
 		if(input.includes("#")) {
-			console.log("Has a #, should be user#0000");
 			const index = input.lastIndexOf("#");
 			discrim = input.substring(index + 1);
 			input = input.substring(0, index);
@@ -24,13 +20,11 @@ module.exports = async (ctx, arg, input) => {
 
 		let user;
 		if(discrim) {
-			console.log("Searching by name & discrim");
 			[user] = await ctx.gatewayRequest().discord().users().query({
 				name: input,
 				discriminator: discrim
 			});
 		} else {
-			console.log("Searching by name");
 			[user] = await ctx.gatewayRequest().discord().users().query("name", input);
 		}
 
